2ed1d6a5708fa3cb09df58f83fba1e356d69825e
zeromake <a390720046@gmail.com>
2021-10-15 00:03:22 +0800

    fix: update zsh use bash -c

--- windows/msys2-zsh.bat
+++ windows/msys2-zsh.bat
@@ -1,11 +1,10 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-set "WD=%~dp0usr\bin\"
-set "LOGINSHELL=zsh"
+set "WD=%~dp0\bin\"
 
 set SHELL_ARGS=%*
 set MSYS=winsymlinks:nativestrict
 set CHERE_INVOKING=1
 
-"%WD%bash" --login -c "%LOGINSHELL%"  !SHELL_ARGS!
+"%WD%bash" -c "zsh" --login !SHELL_ARGS!
